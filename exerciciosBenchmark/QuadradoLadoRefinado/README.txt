0e2d49324484e87108fe77f5c3eb3ae2ad21fbe2
victorrangel10 <victoraugusto.es@gmail.com>
2023-11-12 18:26:03 -0300

    refinado no lado

---
+++ exerciciosBenchmark/QuadradoLadoRefinado/README.txt
@@ -0,0 +1,10 @@
+## Exercício 2
+ Codigo "ex2.c"
+ Dados 5 parametros do tipo double, o programa abre um modelo em forma de quadrado com a malha mais refinada no lado direito.
+ 
+ ### Os 5 parametros sao: 
+ 
+ -mesh size (o tamanho geral da mesh dentro do quadrado)
+ -coordenadas x e y iniciais do quadrado (relativos a aresta inferior esquerda)
+ -tamanho do lado do quadrado
+ -tamanho da mesh no lado
---
+++ exerciciosBenchmark/QuadradoLadoRefinado/ladoRefinado.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+#include "gmshc.h"
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+    
+
+    int ierr;
+
+
+    double lc;
+
+    printf("Select mesh size outside from the line\n");
+
+    scanf("%lf",&lc);
+
+
+    printf("Select square inital coordinates(x,y) and side size\n");
+
+    double initial_x,initial_y,size;
+
+    if(scanf("%lf %lf %lf",&initial_x,&initial_y,&size)!=3){
+        exit(1);
+    }
+ 
+    printf("Select mesh size on the line\n");
+    double new_size;
+    scanf("%lf",&new_size);
+
+
+
+
+    gmshInitialize(argc, argv, 1, 0, &ierr);
+
+    gmshModelAdd("t1", &ierr);
+
+     gmshModelGeoAddPoint(initial_x, initial_y, 0, lc, 1, &ierr);
+    gmshModelGeoAddPoint(initial_x + size, initial_y, 0, lc, 2, &ierr);
+    gmshModelGeoAddPoint(initial_x + size, initial_y+size, 0, lc, 3, &ierr);
+    gmshModelGeoAddPoint(initial_x, initial_y+size, 0, lc, 4, &ierr);
+
+    gmshModelGeoAddLine(1, 2, 1, &ierr);
+    gmshModelGeoAddLine(3, 2, 2, &ierr);
+    gmshModelGeoAddLine(3, 4, 3, &ierr);
+    gmshModelGeoAddLine(4, 1, 4, &ierr);
+
+
+    const int cl1[] = {4, 1, -2, 3};
+    gmshModelGeoAddCurveLoop(cl1, sizeof(cl1) / sizeof(cl1[0]), 1, 0, &ierr);
+
+    const int s1[] = {1};
+    gmshModelGeoAddPlaneSurface(s1, sizeof(s1) / sizeof(s1[0]), 1, &ierr);
+
+    gmshModelGeoSynchronize(&ierr);
+
+
+    gmshModelMeshFieldAdd("Constant", 1, &ierr); // ria campo para adicionar a reta
+
+    const double pl1[] = {2};
+
+    gmshModelMeshFieldSetNumbers(1, "CurvesList", pl1, 1, &ierr); // bota  a reta como pertecente ao campo
+
+    gmshModelMeshFieldSetNumber(1, "VIn", new_size, &ierr); // diz o tamanho do meshsize para a reta
+
+    gmshModelMeshFieldSetAsBackgroundMesh(1, &ierr); // seta o field criado como background mesh
+
+
+
+    //gmshOptionSetNumber("Mesh.MeshSizeExtendFromBoundary", 0, &ierr);
+
+    //gmshOptionSetNumber("Mesh.MeshSizeFromPoints", 0, &ierr);
+
+    //gmshOptionSetNumber("Mesh.MeshSizeFromCurvature", 0, &ierr);
+
+    //gmshOptionSetNumber("Mesh.Algorithm", 5, &ierr);
+
+    gmshModelMeshGenerate(2, &ierr);
+
+    gmshWrite("/home/victor/gmsh/exerciciosBenchmark/QuadradoLadoRefinado/ladoRefinado.msh", &ierr);
+
+    gmshFltkRun(&ierr);
+
+    gmshFinalize(&ierr);
+    return 0;
+}
